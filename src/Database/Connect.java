b2cea75e3cf989f859ca49ab11ccedf7e042dea3
ecbishop1055 <ethancbishop1@gmail.com>
2021-06-04 08:17:27 -0500

    Database Finish - Will continue to add after project is over

--- src/Database/Connect.java
+++ src/Database/Connect.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 public class Connect {
     static String dburl = "jdbc:sqlite:src/Database/database.db";
@@ -18,10 +17,15 @@ public class Connect {
                 System.out.println("Connection to SQLite has been established.");
                 String sql = "CREATE TABLE IF NOT EXISTS Users "
                         + " (id integer USER ID, "
-                        + " username text NOT NULL,"
-                        + " password text NOT NULL)";
+                        + " username text NOT NULL)";
                 Statement stmt = conn.createStatement();
                 stmt.execute(sql);
+
+                String sql2 = "CREATE TABLE IF NOT EXISTS Favorites "
+                        + " (id integer USER ID, "
+                        + " craftFav text NOT NULL,"
+                        + " sprayFav text NOT NULL)";
+                stmt.execute(sql2);
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -33,11 +37,26 @@ public class Connect {
 
 
     }
-    public static void createNewUser(int userID, String username, String password) throws SQLException {
+    public static void createNewUser(int userID, String username) throws SQLException {
         connect();
         var stmt = conn.createStatement();
-        String sql = "INSERT INTO Users (id, username, password) " + "VALUES (" +userID+ ",'" +username+ "', '" + password + "')";
+        String sql = "INSERT INTO Users (id, username) " + "VALUES (" +userID+ ",'" +username+ "')";
         stmt.executeUpdate(sql);
     }
+
+    public static String getUserName(int id) throws SQLException {
+        connect();
+        var stmt = conn.createStatement();
+        String sql = ("SELECT * from Users WHERE (id= " +id+ ")");
+        var results = stmt.executeQuery(sql);
+        if (results.next()){
+            return results.getString("username");
+        } else {
+            System.out.println("No user found!");
+            return null;
+        }
+
+    }
+
 
 }
